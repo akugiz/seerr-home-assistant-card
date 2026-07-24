cfa1dc966cd530a90300c631c3a53cf623544c05
akugiz <158512889+akugiz@users.noreply.github.com>
2026-07-24 15:59:56 +0100

    Add Seerr integration constants

---
+++ custom_components/seerr_card/const.py
@@ -0,0 +1,9 @@
+"""Constants for the Seerr Card integration."""
+
+DOMAIN = "seerr_card"
+CONF_URL = "url"
+CONF_API_KEY = "api_key"
+CONF_VERIFY_SSL = "verify_ssl"
+
+DEFAULT_VERIFY_SSL = True
+API_PREFIX = "/api/v1"
