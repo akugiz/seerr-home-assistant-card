5b6a8132bdd8922dbabd96ba0664597f73eef0ef
akugiz <158512889+akugiz@users.noreply.github.com>
2026-07-25 09:08:20 +0100

    Refresh Seerr card resource cache for 0.2.4

--- custom_components/seerr_card/const.py
+++ custom_components/seerr_card/const.py
@@ -8,6 +8,6 @@ CONF_VERIFY_SSL = "verify_ssl"
 
 DEFAULT_VERIFY_SSL = True
 
-CARD_VERSION = "0.2.2"
+CARD_VERSION = "0.2.4"
 CARD_URL_PATH = "/seerr_card/seerr-search-card.js"
 CARD_RESOURCE_URL = f"{CARD_URL_PATH}?v={CARD_VERSION}"
